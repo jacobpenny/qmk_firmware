32cfa248cdc550d9cfd7aa56b99c36f09d0419ef
Jacob Penny <jacob.penny@gmail.com>
2025-04-22 20:58:18 -0300

    Update

--- keyboards/cantor/keymaps/jp/keymap.c
+++ keyboards/cantor/keymaps/jp/keymap.c
@@ -1,6 +1,7 @@
 // todo
 // next/previous tab in firefox (cmd + left)
 // screenshots / videos
+// min/max wezterm pane combo (shft+ctl+z)
 // qmk info.json vs keyboard.json output wrong
 #include QMK_KEYBOARD_H
 #include "features/achordion.h"
@@ -10,12 +11,20 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-        [0] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, KC_NO, KC_NO, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_NO, KC_NO, LGUI_T(KC_Z), LALT_T(KC_X), LCTL_T(KC_C), LSFT_T(KC_D), ALL_T(KC_V), ALL_T(KC_K), RSFT_T(KC_H), RCTL_T(KC_COMM), RALT_T(KC_DOT), RGUI_T(KC_SLSH), KC_NO, MO(3), LT(1,KC_SPC), KC_TAB, KC_ENT, LT(2,KC_BSPC), KC_DEL),
-        [1] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_AMPR, KC_PLUS, KC_PERC, KC_MINS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_DLR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_CIRC, KC_PGDN, KC_PGUP, KC_ASTR, KC_HASH, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-        [2] = LAYOUT_split_3x6_3(KC_NO, KC_GRV, KC_7, KC_8, KC_9, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EXLM, KC_4, KC_5, KC_6, KC_BSLS, KC_NO, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_AT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SPC, KC_0, KC_NO, KC_NO, KC_NO),
-        [3] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MSTP, KC_MPLY, KC_NO)
+    [0] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, KC_NO, KC_NO, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_NO, KC_NO, LGUI_T(KC_Z), LALT_T(KC_X), LCTL_T(KC_C), LSFT_T(KC_D), ALL_T(KC_V), ALL_T(KC_K), RSFT_T(KC_H), RCTL_T(KC_COMM), RALT_T(KC_DOT), RGUI_T(KC_SLSH), KC_NO, MO(3), LT(1,KC_SPC), KC_TAB, KC_ENT, LT(2,KC_BSPC), KC_DEL),
+    [1] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_AMPR, KC_PLUS, KC_PERC, KC_MINS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_DLR, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_CIRC, KC_PGDN, KC_PGUP, KC_ASTR, KC_HASH, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [2] = LAYOUT_split_3x6_3(KC_NO, KC_GRV, KC_7, KC_8, KC_9, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EXLM, KC_4, KC_5, KC_6, KC_BSLS, KC_NO, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_AT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SPC, KC_0, KC_NO, KC_NO, KC_NO),
+    [3] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MSTP, KC_MPLY, KC_NO)
 };
 
+// Left side combos
+const uint16_t PROGMEM combo_esc[] = {KC_S, KC_T, COMBO_END};
+const uint16_t PROGMEM combo_minus[] = {KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_underscore[] = {KC_R, KC_S, COMBO_END};
+const uint16_t PROGMEM combo_grv[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_interpolate[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM combo_cmdtab[] = {LALT_T(KC_X), LCTL_T(KC_C), COMBO_END};
+// Right side combos
 const uint16_t PROGMEM combo_paren_open[] = {KC_N, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_paren_close[] = {KC_N, KC_I, COMBO_END};
 const uint16_t PROGMEM combo_brace_open[] = {KC_L, KC_U, COMBO_END};
@@ -25,15 +34,12 @@ const uint16_t PROGMEM combo_bracket_close[] = {RSFT_T(KC_H), RALT_T(KC_DOT), CO
 const uint16_t PROGMEM combo_colon[] = {KC_E, KC_I, COMBO_END};
 const uint16_t PROGMEM combo_semi_colon[] = {KC_U, KC_Y, COMBO_END};
 const uint16_t PROGMEM combo_equals[] = {RCTL_T(KC_COMM), RALT_T(KC_DOT), COMBO_END};
-
-const uint16_t PROGMEM combo_esc[] = {KC_S, KC_T, COMBO_END};
-const uint16_t PROGMEM combo_minus[] = {KC_W, KC_F, COMBO_END};
-const uint16_t PROGMEM combo_underscore[] = {KC_R, KC_S, COMBO_END};
-const uint16_t PROGMEM combo_grv[] = {KC_F, KC_P, COMBO_END};
+// Both side combos
+const uint16_t PROGMEM combo_hyperx[] = {KC_Q, KC_QUOT, COMBO_END};
 const uint16_t PROGMEM combo_capsword[] = {KC_W, KC_Y, COMBO_END};
-const uint16_t PROGMEM combo_interpolate[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM combo_cmdtab[] = {LALT_T(KC_X), LCTL_T(KC_C), COMBO_END};
-const uint16_t PROGMEM combo_cmd[] = {LALT_T(KC_X), LCTL_T(KC_C), COMBO_END};
+const uint16_t PROGMEM combo_shiftctrlz[] = {KC_F, KC_U, COMBO_END};
+const uint16_t PROGMEM combo_hypers[] = {KC_P, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_hyperv[] = {KC_B, KC_J, COMBO_END};
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, LT(2,KC_BSPC), KC_DEL);
 
@@ -58,7 +64,11 @@ combo_t key_combos[] = {
     COMBO(combo_grv, KC_GRV),
     COMBO(combo_capsword, QK_CAPS_WORD_TOGGLE),
     COMBO(combo_interpolate, C_INTERPOLATE),
-    COMBO(combo_cmdtab, LGUI(KC_TAB))
+    COMBO(combo_cmdtab, LGUI(KC_TAB)),
+    COMBO(combo_hyperx, HYPR(KC_X)),
+    COMBO(combo_hypers, HYPR(KC_S)),
+    COMBO(combo_hyperv, HYPR(KC_V)),
+    COMBO(combo_shiftctrlz, LSFT(LCTL(KC_Z)))
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
